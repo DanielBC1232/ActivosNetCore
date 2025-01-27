9ede04483259cc968df0e6ae22cee2b881aa9399
Daniel <124007735+DanielBC1232@users.noreply.github.com>
2025-01-26 18:19:36 -0600

    Agregar archivos de proyecto.

---
+++ ActivosNetCore/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActivosNetCore.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+    }
+}
---
+++ ActivosNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ActivosNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
